52e9bdabc92e66016cec330ceab53369f7981d5c
Dmitro Shevchenko <sheva@active-bridge.com>
2020-01-09 14:06:19 +0200

    user-email-confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,4 @@ class UsersController < ApplicationController
   def index
     @users = current_user&.admin? ? User.all : User.guest
   end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
 end
