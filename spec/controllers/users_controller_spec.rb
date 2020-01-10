6e789ba112cdfef3f9a7007c7ba4648674f9095b
Dmitro Shevchenko <sheva@active-bridge.com>
2020-01-10 15:13:24 +0200

    rspec-factory-bot-better-code

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,26 +3,29 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let(:admin) { create(:user, email: 'admin2@example.com', role: :admin) }
+  let(:user) { create(:user, email: 'user3@example.com') }
+
   before do
-    let(:user){create(:user, email: 'user1@example.com', password: 'password', role: :guest)}
-    let(:user){create(:user, email: 'admin1@example.com', password: 'password', role: :admin)}
+    create(:user, email: 'user1@example.com')
+    create(:user, email: 'admin1@example.com', role: :admin)
   end
 
   describe 'GET #index' do
     it 'admin user' do
-      admin = let(:user){create(:user, email: 'admin2@example.com', password: 'password', role: :admin)}
       admin.confirm
       sign_in admin
       get :index, params: {}
       expect(@controller.instance_variable_get(:@users).count).to eq(3)
     end
+
     it 'guest user' do
-      user = let(:user){create (:user, email: 'user3@example.com', password: 'password', role: :guest)}
       user.confirm
       sign_in user
       get :index, params: {}
       expect(@controller.instance_variable_get(:@users).count).to eq(2)
     end
+
     it 'unsigned user' do
       get :index, params: {}
       expect(@controller.instance_variable_get(:@users).count).to eq(1)
