71b3b0fa07f9546ba4fc3a71551e0093b6154ab9
Dmitro Shevchenko <sheva@active-bridge.com>
2020-01-14 16:28:49 +0200

    articles-and-notes

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Article do
-  permit_params :title, :description, :accepted
-
-  index do
-    column :title
-    column :description
-    column :accepted
-  end
+  permit_params :user_id, :title, :description, :accepted
 end
