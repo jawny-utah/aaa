b4d7fd547ab43e315ec1f5fbaaf0d8e1ee5b9487
Dmitro Shevchenko <sheva@active-bridge.com>
2020-01-28 18:55:08 +0200

    add-sorting

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@
 
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.accepted.search_by_content(params[:search]).paginate(page: params[:page]).includes([:user])
+    @articles = Article.accepted.search_by_content(params[:search]).order_list(params[:sort_by])
+                  .paginate(page: params[:page]).includes([:user])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def human_order_articles
+    { 'sort_by_title' => 'Title',
+      'sort_by_description_length' => 'Description',
+      'sort_by_users_email' => 'User Email' }
+  end
 end
