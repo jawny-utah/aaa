f0805109e33c10e8fddc1d33c861fda04007ba6c
Dmitro Shevchenko <sheva@active-bridge.com>
2020-01-29 16:59:58 +0200

    sort-and-search-in-one-form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def human_order_articles
-    { 'sort_by_title' => 'Title',
-      'sort_by_description_length' => 'Description',
-      'sort_by_users_email' => 'User Email' }
+  def order_articles_options
+    [["Sort by", nil],
+     ["Sort by Title", "sort_by_title"],
+     ["Sort by Description", "sort_by_description_length"],
+     ["Sort by User email", "sort_by_users_email"]]
   end
 end
