078d25b84cc3ef080e7957ba6a8b30fe3ab94fbc
Dmitro Shevchenko <sheva@active-bridge.com>
2020-02-18 17:04:54 +0200

    windows.history.pushState

--- app/controllers/hello_world_controller.rb
+++ app/controllers/hello_world_controller.rb
@@ -4,6 +4,9 @@ class HelloWorldController < ApplicationController
   layout 'hello_world'
 
   def index
-    @hello_world_props = { name: current_user&.nickname || 'Stranger', id: current_user&.id }
+    @hello_world_props = { name: current_user&.nickname || 'Stranger', id: current_user&.id,
+                           sort_by: params[:sort_by],
+                           page: params[:page],
+                           user_articles: params[:user_articles] }
   end
 end
